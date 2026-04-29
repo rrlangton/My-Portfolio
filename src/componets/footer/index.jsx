d998a62ecb58927c86850d6235db10045f98302a
Robin <rrlangton@gmail.com>
2026-04-29 18:24:32 -0500

    fix error

--- src/componets/footer/index.jsx
+++ src/componets/footer/index.jsx
@@ -9,7 +9,6 @@ function Footer () {
                             <h6 class="font-weight-bold">ROBIN LANGTON</h6>
                         </li>
                         <li class="list-body">
-                            <p>Business Professional</p><br></br>
                             <a href="https://www.linkedin.com/in/robinrlangton" target="_blank" class="logo">
                                 <img src="assets/imgs/robologo.png" alt="logo image"/>
                                 <h6>HIRE ME</h6>
