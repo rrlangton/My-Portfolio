65a5a7c99ca296f01321cb9d06652d426aa2d095
Robin <rrlangton@gmail.com>
2026-04-29 19:26:24 -0500

    nav bar image added

--- src/componets/nav/index.jsx
+++ src/componets/nav/index.jsx
@@ -3,7 +3,7 @@ function Nav (){
      <nav class="navbar custom-navbar navbar-expand-lg navbar-dark" data-spy="affix" data-offset-top="20">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="assets/imgs/robologo.png" alt="logo image"/>
+                <img src="assets/imgs/robincodednb.png" alt="logo image"/>
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span></span>
